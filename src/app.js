3b5bab5828d8d7cb7bd78855af043b889cbd7af9
necromicom <ebrancatti@gmail.com>
2025-04-06 22:50:26 -0300

    cart

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import { Product } from './prod.js'
 import express from 'express';
+import { Carro } from './cart.js';
 
 
 const app = express();
 const PORT = 8080
 app.use(express.json())
 const product = new Product()
-//getPrpducts
+const cart = new Carro()
+//getProducts
 app.get("/api/products", async(req, res)=>{
     res.send(await product.getProduct()).status(200)
 })
@@ -18,13 +20,13 @@ app.get("/api/products/:pid", async(req, res)=>{
 //post del product
 app.post("/api/products", async(req, res)=>{
     const producto = req.body
-    res.send(await product.addProduct(producto)).status(201)
+    res.send(await product.addProduct(producto))
 })
 //actualizar producto
 app.put("/api/products/:pid", async (req, res) => {
     const pid = req.params.pid
     const updates = req.body
-    res.send(await product.actProd(pid, updates)).status(201)
+    res.send(await product.actProd(pid, updates))
 })
 //delete product
 app.delete("/api/products/:pid", async (req, res) => {
@@ -32,16 +34,29 @@ app.delete("/api/products/:pid", async (req, res) => {
     res.send(await product.borrarProd(pid))
 })
 //crear carro
-app.post("/api/carts/", async (req, res) => {
-    
-
+app.post("/api/carts", async (req, res) => {
+    try {
+        const newCart = await cart.addCart()
+        res.status(201).send(newCart)
+    } catch (err) {
+        console.error("Error al crear el carrito:", err)
+        res.status(500).send({ error: "Error al crear el carrito" })
+    }
 })
 //add2Cart
 app.post("/api/carts/:cid/product/:pid", async (req, res) => {
-    
+    const cid = req.params.cid
+    const pid = req.params.pid
+    const quantity = req.body
+
+    res.send(await cart.addProductInCart(cid, pid, quantity))
 })
-//getCart
+//getCartById
 app.get("/api/carts/:cid", async (req, res) => {
-    
+    const cid = req.params.cid
+    res.send(await cart.getCartById(cid))
+})
+app.get("/api/carts", async (req, res) => {
+    res.send(await cart.getCart())
 })
 app.listen(PORT, ()=>console.log(`escuchando en el ${PORT}`))
--- src/cart.js
+++ src/cart.js
@@ -1,14 +1,56 @@
-export class carro{
-    constructor{
+import fs from "fs"
+export class Carro{
+    constructor(){
         this.path = "./src/carts.json"
     }
+    newId = (c) => {
+        if (c.length > 0){
+            return c[c.length - 1].id +1
+        }else{
+            return 1
+        }
+    }
     addCart = async () => {
-        
+        const cartjson = await fs.promises.readFile(this.path, "utf-8")
+        const cartList = JSON.parse(cartjson)
+        let id = this.newId(cartList)
+        let newCart = {id, products: []}
+        cartList.push(newCart)
+        await fs.promises.writeFile(this.path, JSON.stringify(cartList, null, 2), "utf-8")
+        return newCart
     }
     getCart = async () => {
-        
+        const cartjson = await fs.promises.readFile(this.path, "utf-8")
+        const cartList = JSON.parse(cartjson)
+        return cartList
     }
     getCartById = async (cid) => {
-        
+        const cartjson = await fs.promises.readFile(this.path, "utf-8")
+        const cartList = JSON.parse(cartjson)
+        let cart = cartList.find(c=>c.id===Number(cid))
+        return cart
+
     }
-}   
+    addProductInCart = async (cid, pid, quantityObj) => {
+        const cartjson = await fs.promises.readFile(this.path, "utf-8");
+        const cartList = JSON.parse(cartjson);
+    
+        const cartIndex = cartList.findIndex(c => c.id === Number(cid));
+        if (cartIndex === -1) {
+            console.log("Carrito no encontrado");
+            return null;
+        }
+    
+        const cart = cartList[cartIndex];
+        const productIndex = cart.products.findIndex(p => p.id === Number(pid));
+        const quantity = quantityObj.quantity;     
+        if (productIndex !== -1) {
+            cart.products[productIndex].quantity += quantity;
+        } else {
+            cart.products.push({ id: Number(pid), quantity });
+        }
+        await fs.promises.writeFile(this.path, JSON.stringify(cartList, null, 2), "utf-8");
+        return cart;
+    };
+    
+}
